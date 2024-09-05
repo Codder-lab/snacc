fa649c268bccf79168234cef50a1b25689cb5d05
suyashpotdar <suyashpotdar03@gmail.com>
2024-09-06 00:15:48 +0530

    add backend url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = 'https://snacc.onrender.com';
+  const url = 'https://snacc-backend.onrender.com';
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
